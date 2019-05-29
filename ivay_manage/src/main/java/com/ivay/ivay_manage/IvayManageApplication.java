8115ed818bcc4c632959b103fe0bb910cea3dc82
Anime <577361902@qq.com>
2019-05-29 21:10:04 +0800

    com模块增加配置

--- ivay_manage/src/main/java/com/ivay/ivay_manage/IvayManageApplication.java
+++ ivay_manage/src/main/java/com/ivay/ivay_manage/IvayManageApplication.java
@@ -3,7 +3,8 @@ package com.ivay.ivay_manage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// 同时注入多个模块的bean时，不同bean的包名必须相同
+@SpringBootApplication(scanBasePackages = "com.ivay")
 public class IvayManageApplication {
     public static void main(String[] args) {
         SpringApplication.run(IvayManageApplication.class, args);
