23aab9dbc4211ec48871376008b29d1290589754
panshuhua <544341796@qq.com>
2019-06-14 10:30:10 +0800

    添加firebase定时器cron表达式

--- ivay_app/src/main/java/com/ivay/ivay_app/job/FirebaseScheduleTask.java
+++ ivay_app/src/main/java/com/ivay/ivay_app/job/FirebaseScheduleTask.java
@@ -1,7 +1,6 @@
 package com.ivay.ivay_app.job;
 
 import com.ivay.ivay_app.service.XFirebaseNoticeService;
-import com.ivay.ivay_app.service.XRecordLoanService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
