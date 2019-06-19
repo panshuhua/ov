0075ed27123f927c7cec5e03161dfab61dfeaf71
panshuhua <544341796@qq.com>
2019-06-19 17:59:28 +0800

    ok

--- ivay_app/src/main/java/com/ivay/ivay_app/service/impl/XUserIContactsServiceImpl.java
+++ ivay_app/src/main/java/com/ivay/ivay_app/service/impl/XUserIContactsServiceImpl.java
@@ -14,14 +14,20 @@ import com.ivay.ivay_repository.model.XUserAppNum;
 import com.ivay.ivay_repository.model.XUserContacts;
 import com.ivay.ivay_repository.model.XUserInfo;
 import com.ivay.ivay_repository.model.XUserRisk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 @Service
 public class XUserIContactsServiceImpl implements XUserContactsService {
+	private static final Logger logger = LoggerFactory.getLogger("adminLogger");
+	
     @Autowired
     private XUserContactsDao xUserContactsDao;
     @Autowired
@@ -44,7 +50,7 @@ public class XUserIContactsServiceImpl implements XUserContactsService {
         xUserRisk.setUpdateTime(new Date());
 
         //联系人：每天都要上传记录
-        if(SysVariable.CONTACT.equals(type)) {
+//        if(SysVariable.CONTACT.equals(type)) {
         	 Set<XUserContacts> contacts = riskInfo.getContacts();
              Set<XUserContacts> xUserContacts = new HashSet();
              //查询当天该用户有没有上传通讯录，如果上传了就不再重复上传了 
@@ -69,10 +75,10 @@ public class XUserIContactsServiceImpl implements XUserContactsService {
                      }
                  }
              }
-        }
+//        }
        
         //社交类app的个数-每天都要记录
-        if(SysVariable.APPNUM.equals(type)) {
+//        if(SysVariable.APPNUM.equals(type)) {
         	xUserInfo.setAppNum(riskInfo.getAppNum());
         	XUserAppNum xUserAppNum=new XUserAppNum();
         	xUserAppNum.setAppNum(riskInfo.getAppNum());
@@ -85,39 +91,57 @@ public class XUserIContactsServiceImpl implements XUserContactsService {
         		xUserAppNumDao.saveAppNum(xUserAppNum);
         	}
         	
-        }
+//        }
         
         //查询风控数据表有无该gid的用户，如果没有就新增一条记录
         Integer userNum = xUserRiskDao.findUser(gid);
-        xUserRisk.setLongitude(riskInfo.getLongitude());
-    	xUserRisk.setLatitude(riskInfo.getLatitude());
-        xUserRisk.setMacAddress(riskInfo.getMacAddress());
-        xUserRisk.setPhoneBrand(riskInfo.getPhoneBrand());
-        xUserRisk.setTrafficWay(riskInfo.getTrafficWay());
+        BigDecimal longitude = riskInfo.getLongitude();
+        BigDecimal latitude = riskInfo.getLatitude();
+        String macAddress=riskInfo.getMacAddress();
+        String phoneBrand = riskInfo.getPhoneBrand();
+        String trafficWay = riskInfo.getTrafficWay();
+        xUserRisk.setLongitude(longitude);
+    	xUserRisk.setLatitude(latitude);
+        xUserRisk.setMacAddress(macAddress);
+        xUserRisk.setPhoneBrand(phoneBrand);
+        xUserRisk.setTrafficWay(trafficWay);
         
         //gps位置信息
-        if(SysVariable.GPS.equals(type)) {
+//        if(SysVariable.GPS.equals(type)) {
         	//xUserInfo.setLongitude(riskInfo.getLongitude());
             //xUserInfo.setLatitude(riskInfo.getLatitude());
             //xUserInfoDao.updateGps(xUserInfo);
         	
+            logger.info("风控表中已有的数据个数："+userNum+"-------------------");
         	//经纬度信息移动到风控表
-        	if(userNum == 0) {
-        		xUserRiskDao.save(xUserRisk);
-        	}else {
-        		xUserRiskDao.updateGpsInfo(xUserRisk);
-        	}
+            if(!StringUtils.isEmpty(longitude) && !StringUtils.isEmpty(latitude)) {
+            	if(userNum == 0 || userNum==null) {
+           		    logger.info("保存风控表数据gps："+userNum+"-------------------");
+           		    xUserRiskDao.save(xUserRisk);
+	           	}else {
+	           		 logger.info("更新风控表数据gps："+userNum+"-------------------");
+	           		 xUserRiskDao.updateGpsInfo(xUserRisk);
+	           	}
+            	
+            }
+        	
             
-        }
+//        }
         
         //mac地址等其他信息
-        if(SysVariable.OTHER.equals(type)) {
-        	if(userNum == 0) {
-        		xUserRiskDao.save(xUserRisk);
-        	}else {
-        		xUserRiskDao.updateOthers(xUserRisk);
-        	}
-        }
+            userNum = xUserRiskDao.findUser(gid);
+//        if(SysVariable.OTHER.equals(type)) {
+            if(!StringUtils.isEmpty(macAddress) || !StringUtils.isEmpty(phoneBrand) || !StringUtils.isEmpty(trafficWay)) {
+            	if(userNum == 0 || userNum==null) {
+           		  logger.info("保存风控表数据other："+userNum+"-------------------");
+           		  xUserRiskDao.save(xUserRisk);
+           	 }else {
+           		  logger.info("保存风控表数据other："+userNum+"-------------------");
+           		  xUserRiskDao.updateOthers(xUserRisk);
+           	  }
+          }
+        	
+//        }
         
         return true;
     }
