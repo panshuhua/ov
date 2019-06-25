f4469755cf9d2f52238458bfdfb72033fda2fcdc
Anime <577361902@qq.com>
2019-06-25 11:08:21 +0800

    逾期bug

--- ivay_repository/src/main/java/com/ivay/ivay_repository/dao/master/XRecordLoanDao.java
+++ ivay_repository/src/main/java/com/ivay/ivay_repository/dao/master/XRecordLoanDao.java
@@ -1,17 +1,11 @@
 package com.ivay.ivay_repository.dao.master;
 
+import com.ivay.ivay_repository.model.XRecordLoan;
+import org.apache.ibatis.annotations.*;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-
-import com.ivay.ivay_repository.model.XRecordLoan;
 
 @Mapper
 public interface XRecordLoanDao {
@@ -28,20 +22,29 @@ public interface XRecordLoanDao {
 
     @Options(useGeneratedKeys = true, keyProperty = "id")
     @Insert("insert into x_record_loan(gid, order_id, user_gid, bankcard_gid, product_id, loan_amount,loan_period, loan_rate, loan_time,"
-        + "fee,interest,memo, net_amount, due_amount, due_time,overdue_fee,overdue_fee_total, overdue_interest,overdue_interest_total,"
-        + "loan_status, repayment_status,last_repayment_time, fail_reason,create_time, update_time)"
-        + "values(#{gid}, #{orderId}, #{userGid}, #{bankcardGid },#{productId},#{loanAmount},#{loanPeriod}, #{loanRate}, #{loanTime},"
-        + "#{fee},#{interest},#{memo}, #{netAmount},#{dueAmount}, #{dueTime}, #{overdueFee},#{overdueFeeTotal},#{overdueInterest}, #{overdueInterestTotal},"
-        + "#{loanStatus}, #{repaymentStatus}, #{lastRepaymentTime}, #{failReason},#{createTime}, #{updateTime})")
+            + "fee,interest,memo, net_amount, due_amount, due_time,overdue_fee,overdue_fee_total, overdue_interest,overdue_interest_total,"
+            + "loan_status, repayment_status,last_repayment_time, fail_reason,create_time, update_time)"
+            + "values(#{gid}, #{orderId}, #{userGid}, #{bankcardGid },#{productId},#{loanAmount},#{loanPeriod}, #{loanRate}, #{loanTime},"
+            + "#{fee},#{interest},#{memo}, #{netAmount},#{dueAmount}, #{dueTime}, #{overdueFee},#{overdueFeeTotal},#{overdueInterest}, #{overdueInterestTotal},"
+            + "#{loanStatus}, #{repaymentStatus}, #{lastRepaymentTime}, #{failReason},#{createTime}, #{updateTime})")
     int save(XRecordLoan xRecordLoan);
 
     int count(@Param("params") Map<String, Object> params);
 
-    List<XRecordLoan> list(@Param("params") Map<String, Object> params, @Param("offset") Integer offset,
-        @Param("limit") Integer limit);
+    /**
+     * 找出某个用户的所有借款成功的记录，会置顶未还清借款
+     *
+     * @param params
+     * @param offset
+     * @param limit
+     * @return
+     */
+    List<XRecordLoan> list(@Param("params") Map<String, Object> params,
+                           @Param("offset") Integer offset,
+                           @Param("limit") Integer limit);
 
     @Select("select IFNULL(SUM(t.due_amount+t.overdue_fee+t.overdue_interest),0) from x_record_loan t"
-        + " where t.user_gid = #{userGid} AND t.loan_status='1' AND (t.repayment_status ='0' OR t.repayment_status='1')")
+            + " where t.user_gid = #{userGid} AND t.loan_status='1' AND (t.repayment_status ='0' OR t.repayment_status='1')")
     long getSumLoanAmount(String userGid);
 
     @Select("SELECT min(x.last_repayment_time) FROM x_record_loan x WHERE x.user_gid=#{userGid} AND x.repayment_status='2'")
