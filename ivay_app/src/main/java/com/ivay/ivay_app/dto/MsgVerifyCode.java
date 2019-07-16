ad7037d482a725520901c814035c290050e54bb2
panshuhua <544341796@qq.com>
2019-07-16 14:20:26 +0800

    删除app多余代码

--- ivay_app/src/main/java/com/ivay/ivay_app/dto/MsgVerifyCode.java
+++ ivay_app/src/main/java/com/ivay/ivay_app/dto/MsgVerifyCode.java
@@ -1,11 +1,16 @@
 package com.ivay.ivay_app.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class MsgVerifyCode {
+    @ApiModelProperty("手机号码")
     private String mobile;
+    @ApiModelProperty("短信验证码")
     private String authCode;
+    @ApiModelProperty("手机短信")
     private String phoneMsg;
+    @ApiModelProperty("发送方式")
     private String sendMethod; // 发送方式
 }
