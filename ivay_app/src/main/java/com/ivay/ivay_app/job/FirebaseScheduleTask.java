aa26bee0362adce699e7c30871ba1a692eccf7cb
panshuhua <544341796@qq.com>
2019-07-25 13:54:20 +0800

    定时任务删除多余注解

--- ivay_app/src/main/java/com/ivay/ivay_app/job/FirebaseScheduleTask.java
+++ ivay_app/src/main/java/com/ivay/ivay_app/job/FirebaseScheduleTask.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +13,6 @@ import com.ivay.ivay_common.utils.RedisLock;
 
 @Component
 @Configuration
-@EnableScheduling
 public class FirebaseScheduleTask {
     private static final Logger logger = LoggerFactory.getLogger(FirebaseScheduleTask.class);
 
